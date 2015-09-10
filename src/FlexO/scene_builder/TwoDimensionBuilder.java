104e5c129355d7a9ee48d9c3b6d1ad1266130bc4
Piotr Baran <pbaran@student.agh.edu.pl>
2015-09-10 16:48:30 +0200

    Why wasn't this commited in previous commit?

---
+++ src/FlexO/scene_builder/TwoDimensionBuilder.java
@@ -0,0 +1,11 @@
+package FlexO.scene_builder;
+
+/**
+ * Created by Piotr on 2015-09-10.
+ */
+public class TwoDimensionBuilder implements SceneBuilder {
+    @Override
+    public void build() {
+
+    }
+}
